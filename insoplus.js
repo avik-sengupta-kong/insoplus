7f09a9fd633db3b680b61e8895cd48f025c89858
avik-sengupta-kong <avik.sengupta@konghq.com>
2021-09-29 10:56:07 +0100

    Added route conflict option

--- insoplus.js
+++ insoplus.js
@@ -2,6 +2,7 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 // require('dotenv').config();
 const path= require('path');
+const { Console } = require('console');
 
 
 
@@ -21,20 +22,50 @@ if(command==''){
 var specPath = path.resolve("./",spec); 
 console.log('Kong conf yaml path ' + specPath);
 
-var conf = yaml.load(fs.readFileSync(specPath, "utf8"));
+try
+{
+    var conf = yaml.load(fs.readFileSync(specPath, "utf8"));
 
-if(command =='remove-upstream'){
+    switch(command)
+    {
+        case 'remove-upstream':
+            removeUpstream(conf);
+            break;
+        case 'add-route-header':
+            addRouteHeader(conf);
+            break;
+        case 'add-route-host':
+                addRouteHost(conf);
+                break;
+        case 'check-route':
+                checkRouteConflict(conf);
+                break;
+        default:
+            console.log("No valid option is selected. Valid options are one of '\n' remove-upstream '\n' add-route-header '\n' add-route-host '\n' check-route-conflict");
+            process.exit(1);
+    } 
+}catch(e)
+{
+    console.error(e);
+    process.exit(1);
+}
+
  
+
+//console.log(yaml.dump(conf));
+fs.writeFileSync(path.resolve("./", 'insoplus_' +spec), yaml.dump(conf));
+console.log("new config file is written in insoplus_" + spec );
+
+
+function removeUpstream(conf)
+ {
     
     console.log( "this will remove all upstream and associated targets and replace service.host with the first target. Only use this if you have a load balancer pointing to the target");
-    
-
     if(!conf.upstreams){
         console.log( "No upstreams present");
         process.exit(1);
 
-    }
-
+    } 
     if(conf.upstreams.length==0){
         console.log( "No upstreams present")
         process.exit(1);
@@ -51,36 +82,35 @@ if(command =='remove-upstream'){
     conf.services[0].host = firstTarget.split(":")[0];
     delete conf.upstreams;
 
+
  
 }
 
+function addRouteHeader(conf){
 
-if(command =='add-route-header'){
- 
-  
-   var header = process.argv[4]?process.argv[4]:'';
+    var header = process.argv[4]?process.argv[4]:'';
 
-   if(header==''){
+    if(header==''){
     console.log("expecting header name and value as 3rd argument, separated by colon");
     process.exit(1);
-   }
-   var headerValues = header.split(":");
-   if(headerValues.length!=2){
+    }
+    var headerValues = header.split(":");
+    if(headerValues.length!=2){
     console.log("expecting header name and value as 3rd argument, separated by colon. For example x-service-name:myservice");
     process.exit(1);
-   }
+    }
 
-   var routeName = process.argv[5]?process.argv[5]:'';
-   if(routeName!=''){
+    var routeName = process.argv[5]?process.argv[5]:'';
+    if(routeName!=''){
     console.log("Route Name supplied. New header " + header + " will only be added for route " + routeName);
     
-   }else{
+    }else{
     console.log("No route Name supplied. New header " + header + " will be added for all routes for first service");
-   }
+    }
 
 
 
-   
+
     conf.services[0].routes.map(x=>addHeaders(x, headerValues[0], headerValues[1], routeName));
 
     function addHeaders(route, headerName, headerValue, routeName) {
@@ -88,12 +118,10 @@ if(command =='add-route-header'){
             route.headers = { [headerName]: [headerValue]} ;
         }
         return route;
-  }
+    }
 }
 
-if(command =='add-route-host'){
- 
-  
+function addRouteHost(conf){
     var route_host = process.argv[4]?process.argv[4]:'';
  
     if(route_host==''){
@@ -118,8 +146,60 @@ if(command =='add-route-host'){
          }
          return route;
    }
- }
+}
 
-console.log(yaml.dump(conf));
-fs.writeFileSync(path.resolve("./", 'insoplus_' +spec), yaml.dump(conf));
-console.log("new config file is written in insoplus_" + spec );
+
+function checkRouteConflict(conf)
+ {
+    
+    console.log( "this will check all existing routes and compare them with inso config to ensure that will not be any conflicting route.");
+    console.warn( "Ensure that supplied dump file is for the whole workspace.");
+    var dumpFile = process.argv[4]?process.argv[4]:'';
+    if(dumpFile==''){
+        console.log("expecting Kong dump file name as 3rd argument");
+        process.exit(1);
+       }
+
+    var dumpPath = path.resolve("./",dumpFile); 
+    console.log(" deck dump file :" + dumpPath);
+    var dump = yaml.load(fs.readFileSync(dumpPath, "utf8"));
+    
+    // looping through dump file to get all routes in an array.
+    var dumpRoutes = [];
+    // compares only the first set of route attributes. Using Inso generate config there is limited option to declare more.
+    dump.services.forEach(s=> {
+        s.routes.forEach(r=>{
+         var path = r.paths?r.paths[0]:'';
+         var host = r.hosts?r.hosts[0]:'';
+         var method = r.methods?r.methods[0]:'';
+         var header = r.headers?r.headers[0]:'';
+
+         dumpRoutes.push({path:path,host:host, method:method, header: header});
+
+        })
+    });
+    // now looping through spec routes to see if there is any conflict.
+    conf.services[0].routes.forEach( cr => {
+        var specpath = cr.paths?cr.paths[0]:'';
+        var spechost = cr.hosts?cr.hosts[0]:'';
+        var specmethod = cr.methods?cr.methods[0]:'';
+        var specheader = cr.headers?cr.headers[0]:'';
+
+        var conflict = dumpRoutes.filter( d=> (d.path == specpath && d.host == spechost && d.method == specmethod && d.header == specheader ));
+        if (conflict.length>0) {
+            console.warn("Following routes already exists in your workspace. Please fix them before you run deck sync")
+            conflict.forEach( con =>{
+                console.warn( "Route detail: " + JSON.stringify(con));
+            })
+            console.warn( conflict);
+        }
+    
+    })
+
+    console.log("program exiting now."); process.exit(1);
+
+    
+
+
+ 
+}
--- readme.md
+++ readme.md
@@ -7,31 +7,62 @@ What the helper function can do.
 1. Remove upstreams when they are not needed.
 2. Add Route heders for either specific or for all routes.
 3. Add Route host for either specific or for all routes. 
+4. Check conflicting routes
 
-Install
-Save this localally and either add this in the path or call using the full path.
+**Install**  
+Save this localally and either add this file in the API repository or call using the full path.
 Before first use, need to call npm install that will install the libraries/node modules.
 
 N.B. InsoPlus will only work if there's one service on the kong.yaml.
 
 **Usage**
 
-**Step 1:** Run inso generate config. This will generate the output file, normally named as kong.yaml.
+To add/remove elements from Kong conf
+
+**Step 1:** Run inso generate config . This will generate the output file, normally named as kong.yaml.
 
 **Step 2:** Now run insoplus for additional configuration. This will generate a new yaml named insoplus_kong.yaml
 
-**Examples**
-```
-node insoplus.js kong.yaml remove-upstream ( this will remove upstream object and replace service.host with the first target found in upstream)   
-node insoplus.js kong.yaml add-route-header ( this will add a route header for all routes found in service)   
-node insoplus.js kong.yaml add-route-header route-name ( this will add a route header for the route named route-name)  
-node insoplus.js kong.yaml add-route-host ( this will add a route host for all routes found in service)   
-node insoplus.js kong.yaml add-route-host route-name ( this will add a route host for the route named route-name)  
-```
-**Examples**
+<span style="color:blue">
 
+<i>
+
+node insoplus.js sample_kong_conf remove-upstream ( this will remove upstream object and replace service.host with the first target found in upstream)  
+
+node insoplus.js sample_kong_conf add-route-header x-service-name:myservice ( this will add a route header called x-service-name with value of myservice for all routes found in service)  
+
+node insoplus.js sample_kong_conf add-route-header x-service-name:myservice route-name  ( this will add a route header for the route named route-name)  
+
+node insoplus.js sample_kong_conf add-route-host host-name ( this will add a route host for all routes found in service)  
+
+node insoplus.js sample_kong_conf add-route-host host-name route-name ( this will add a route host for the route named route-name)  
+
+</i>
+
+</span>
 
 **Step 3**: Run Deck Sync to upload the new config insoplus_kong.yaml to Kong Manager.
+
+To check route conflicts from Kong conf and deck dump
+
+**Step 1:** Run inso generate config . This will generate the output file, normally named as kong.yaml.
+
+**Step 2**: Run Deck dump on the whole workspace.
+
+**Step 3:** Run insoplus to check route conflict, then it will just output conflicted routes, and will not generate a new yaml.
+
+<span style="color:blue">
+
+<i>
+
+node insoplus.js sample_kong_conf check-route sample_deck-dump.yaml ( compare service routes in the kong configuration with existing routes placed in deck dump file)
+</i>
+
+</span>
+
+**Sample**
+
+There is a sample kong config ( sample_kong_conf) that can be used to test and have a play with insoplus.
 
 
 
